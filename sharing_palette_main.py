67826068c5e72f56d6e1cb0097709fb5b36c0f6f
Rocki_Kim <rocki.k1208@gmail.com>
2024-01-31 23:47:35 +0900

    FEAT Change the images

--- sharing_palette_main.py
+++ sharing_palette_main.py
@@ -14,13 +14,13 @@ def all_img_collecter(file_path):
     return files       
 
 # Function to load and display images in a grid
-def display_images(selected_directory):
-    selected_directory_path = join(img_data, selected_directory)
+def display_images(selected_directory_path):
+    # selected_directory_path = join(img_data, selected_directory)
     raw_imgs = all_img_collecter(img_data+"/raw_face")
     st.write("## Before 이미지")
     st.image(Image.open(raw_imgs[0]), width=300)
     images = all_img_collecter(selected_directory_path)
-    st.write(f"# {selected_directory}")
+    # st.write(f"# {selected_directory}")
     images = sorted(images, key=natural_sort_key)
     if selected_directory == 'checco_ver_1_snap':
         st.write("\n\n")
@@ -33,15 +33,15 @@ def display_images(selected_directory):
         with col1:
             if i < len(images):
                 image_path = row[0]
-                img_cap = int(image_path.split('/')[-1].split('.')[0]) + 1 
+                img_cap = int(image_path.split('/')[-1].split('.')[0]) 
                 if selected_directory != 'checco_ver_1_snap':
                     st.write(f"### {img_cap} 번")
                 st.image(Image.open(image_path), use_column_width=True)
 
         with col2:
-            if i + 1 < len(images):
+            if i < len(images):
                 image_path = row[1]
-                img_cap = int(image_path.split('/')[-1].split('.')[0]) + 1 
+                img_cap = int(image_path.split('/')[-1].split('.')[0]) 
                 if selected_directory != 'checco_ver_1_snap':
                     st.write(f"### {img_cap} 번")
                 st.image(Image.open(image_path), use_column_width=True)
@@ -52,31 +52,40 @@ img_data = "./img_data"  # Replace with the actual path
 # Streamlit app
 st.title("Checco Palette Viewer")
 
-# Selectbox to choose the image directory
-selected_palette = st.selectbox("확인할 팔레트를 선택해주세요.", ["MOONLIGHT", "CHRISTMAS", "TENNIS", "FOUR SEASONS"])
 
-dir_dict = {'MOONLIGHT': 'night_snap_retouched',
-            'CHRISTMAS': 'christmas_snap_retouched',
-            'TENNIS': 'tennis_snap_retouched',
-            'FOUR SEASONS': 'phase_1'}
-
-selected_directory = dir_dict.get(selected_palette)
 
 # Radio button to choose image source
-image_source = st.radio("원본을 고를지, 적용된 이미지(이효리)를 고를지 선택해주세요.", ["中条あやみ", "이효리", "checco Model 1", "checco Model 2"])
+image_source = st.radio("원본을 고를지, 적용된 이미지(이효리)를 고를지 선택해주세요.", ["이효리", "model_8", "ai_model_D"])
+img_data = join(img_data, image_source+"_result")
+palette_lst = sorted(os.listdir(img_data))
+# Selectbox to choose the image directory
+selected_palette = st.selectbox("확인할 팔레트를 선택해주세요.", palette_lst)
+selected_directory = join(img_data, selected_palette)
 
-# Set image directory based on the selected source
-if image_source == "이효리":
-    img_data = join(img_data, 'hyoree_1_test_result')
-elif image_source == "中条あやみ":
-    img_data = join(img_data, '中条あやみ_data')
-elif image_source == "checco Model 1":
-    img_data = join(img_data, 'checco_model_1_data')
-elif image_source == "checco Model 2":
-    img_data = join(img_data, 'ai_model_short_test_result')
-# elif image_source == "checco Model 3":
-#     img_data = join(img_data, 'ai_model_D_test_result')
-# Display images based on the selected directory
+# dir_dict = {'MOONLIGHT': 'night_snap_retouched',
+#             'graduation': 'graduation_snap',
+#             'hanbok': 'hanbok_snap',
+#             'japan_idol': 'japan_idol_snap',
+#             'kimono': 'kimono_snap',
+#             'graduation': 'graduation_snap',
+#             'graduation': 'graduation_snap',
+#             'TENNIS': 'tennis_snap_retouched',
+#             'FOUR SEASONS': 'phase_1'}
+
+# selected_directory = dir_dict.get(selected_palette)
+
+# # Set image directory based on the selected source
+# if image_source == "이효리":
+#     img_data = join(img_data, 'model_8_result')
+# elif image_source == "中条あやみ":
+#     img_data = join(img_data, '中条あやみ_data')
+# elif image_source == "checco Model 1":
+#     img_data = join(img_data, 'checco_model_1_data')
+# elif image_source == "checco Model 2":
+#     img_data = join(img_data, 'ai_model_short_test_result')
+# # elif image_source == "checco Model 3":
+# #     img_data = join(img_data, 'ai_model_D_test_result')
+# # Display images based on the selected directory
 
 
 
