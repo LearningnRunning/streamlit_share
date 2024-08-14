ff23ac0f7568ad93eadf8caff87e05944b277236
Rocki_Kim <rocki.k1208@gmail.com>
2024-08-14 09:16:39 +0900

    ADD all country word2vec data

--- find_key_in_df_streamlit.py
+++ find_key_in_df_streamlit.py
@@ -16,21 +16,22 @@ def find_rows(df, find_key):
     else:
         return pd.DataFrame()
 
+def extract_country(filename):
+    match = re.search(r'_(japan|korea|american|china)_', filename, re.IGNORECASE)
+    if match:
+        return match.group(1).capitalize()
+    return 'Unknown'
+    
 @st.cache_data
 def load_csv(file_list):
-    def extract_country(filename):
-        match = re.search(r'_(japan|korea|american|china)_', filename, re.IGNORECASE)
-        if match:
-            return match.group(1).capitalize()
-        return 'Unknown'
-    
+  
     file_dict = {extract_country(os.path.basename(file)): pd.read_csv(file) for file in file_list}
     return file_dict
 
 @st.cache_data
-def load_word2vec(model_file_path):
-    model = Word2Vec.load(model_file_path)
-    return model
+def load_word2vec(model_file_path_list):
+    file_dict = {extract_country(os.path.basename(file)): Word2Vec.load(file) for file in model_file_path_list}
+    return file_dict
 
 @st.cache_data
 def load_excel(file_path):
@@ -39,6 +40,13 @@ def load_excel(file_path):
 excel_path = os.path.join(STATIC_DATA_PATH, 'country_keyword_frequency.xlsx')
 sheets = load_excel(excel_path)
 
+csv_file_list = glob(os.path.join(NOW_PATH, STATIC_DATA_PATH, 'only_sentences', '*.csv'))
+df_dict = load_csv(csv_file_list)
+
+
+model_files = glob(os.path.join(STATIC_DATA_PATH, 'word2vec', '*word2vec_model'))
+vec_model_dict = load_word2vec(model_files)
+    
 st.sidebar.title('메뉴 선택')
 app_mode = st.sidebar.radio('모드를 선택하세요:', ['국가별 Keyword 빈도수', 'Keyword가 포함된 문장 찾기', 'Keyword 연관 단어 찾기'])
 
@@ -58,10 +66,7 @@ if app_mode == '국가별 Keyword 빈도수':
 elif app_mode == 'Keyword가 포함된 문장 찾기':
     st.title('Keyword가 포함된 문장 찾기')
 
-    file_list = glob(os.path.join(NOW_PATH, STATIC_DATA_PATH, '*.csv'))
-    df_dict = load_csv(file_list)
-
-    selected_file_name = st.selectbox('CSV 파일을 선택하세요:', list(df_dict.keys()))
+    selected_file_name = st.selectbox('국가를 선택하세요:', list(df_dict.keys()))
     if selected_file_name:
         selected_df = df_dict[selected_file_name]
 
@@ -81,8 +86,10 @@ elif app_mode == 'Keyword가 포함된 문장 찾기':
 elif app_mode == 'Keyword 연관 단어 찾기':
     st.title('Keyword 연관 단어 찾기')
 
-    china_word2vec_model_file_path = os.path.join(STATIC_DATA_PATH, 'china', 'intern_china_word2vec_model')
-    vec_model = load_word2vec(china_word2vec_model_file_path)
+    selected_file_name = st.selectbox('국가를 선택하세요:', list(vec_model_dict.keys()))
+    if selected_file_name:
+        vec_model = vec_model_dict[selected_file_name]
+
 
     key_word = st.text_input('키워드를 입력하세요:')
     top_num = st.slider(label='연관 단어 수', min_value=1, max_value=100, value=5)
